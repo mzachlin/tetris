cf0873167d52d766c17058f451bf9bc16f13aa77
clink4 <43253742+clink4@users.noreply.github.com>
2021-02-23 19:43:25 -0500

    Update piece.cpp

--- piece.cpp
+++ piece.cpp
@@ -1,74 +1,95 @@
+#include "piece.h"
 #include "grid.h"
 
-Grid::Grid(int width, int height, int block_size) {
-  g_x_num = width/block_size;
-  g_y_num = height/block_size;
-  g_block_size = block_size;
-
-  //create blank grid
-  for (int i = 0; i <= g_y_num; i++) {
-    vector<box> row;
-    for (int k = 0; k <= g_x_num; k++ ) {
-      SDL_Rect r1;
-      box one_box = {r1, false};
-      row.push_back(one_box);
-    }
-    grid_blocks.push_back(row);
-  }
-}
-
-void Grid::UpdateGrid(Piece piece) {
-  vector<SDL_Rect> blocks = piece.Get_Blocks();
+// Piece constructer
+Piece::Piece(int block_size, int win_w, int win_h) {
+  p_block_size = block_size;
+  p_win_w = win_w;
+  p_win_h = win_h;
+  locked = false;
+  //create shape with blocks
   for (int i = 0; i < 4; i++) {
-    SDL_Rect rect = blocks[i];
-    int x = rect.x/piece.GetBlockSize();
-    int y = rect.y/piece.GetBlockSize();
-    box b = {rect, true};
-    if (y >= g_y_num || x >= g_x_num) {
-      cout << "wrong! x is " << x << " and y is " << y << endl;
-    }
-    
-    grid_blocks[y][x] = b;
+    // Create rectangle
+    SDL_Rect rect;
+    rect.w = p_block_size;
+    rect.h = p_block_size;
+    // Set initial x and y position
+    rect.x = p_win_w/2-p_block_size;
+    rect.y = p_block_size*i;
+    blocks.push_back(rect);
   }
+
 }
 
-vector<vector<box> > Grid::GetGridBlocks() {
-  return grid_blocks;
+int Piece::GetWinWidth() {
+  return p_win_w;
 }
 
-bool Grid::isOccupied(int x, int y, int block_size) {
-  x = x/block_size;
-  y = y/block_size;
-  return grid_blocks[y][x].filled;
+int Piece::GetWinHeight() {
+  return p_win_h;
 }
 
-void Grid::CheckRows() {
-  for (int i = 0; i <= g_y_num; i++){
-    bool isFull = true;
-    for (int k = 0; k < g_x_num; k++) {
-        if (!grid_blocks[i][k].filled) {
-          isFull = false;
-          break;
-        }
+int Piece::GetBlockSize() {
+  return p_block_size;
+}
 
+//member functions
+vector<SDL_Rect> Piece::Get_Blocks() {
+  return blocks;
+}
+
+bool Piece::isLocked() {
+  return locked;
+}
+
+bool Piece::OutOfBounds(bool checkDown, bool checkLeft, bool checkRight, Grid *grid) {
+  for (int i = 0; i < 4; i++) {
+    if (checkDown && (blocks[i].y + blocks[i].h > p_win_h || grid->isOccupied(blocks[i].x, blocks[i].y, blocks[i].h))) {
+      cout << "occupied; x is " << blocks[i].x << " y is " << blocks[i].y << endl;
+      return true;
     }
-    if (isFull) {
-      //copy down all the rows above; decrement their positions
-      for (int k = i; k > 0; k--) {
-          for (int j = 0; j < g_x_num; j++) {
-            grid_blocks[k][j] = grid_blocks[k-1][j];
-            if (grid_blocks[k-1][j].filled) {
-              grid_blocks[k][j].rect.y += g_block_size;
-            }
-          }
-      }
-      //blank out top row
-      for (int j = 0; j < g_x_num; j++) {
-        grid_blocks[0][j].filled = false;
-      }
+    if (checkLeft && (blocks[i].x < 0 || grid->isOccupied(blocks[i].x, blocks[i].y, blocks[i].h))) {
+      return true;
+    }
+    if (checkRight && (blocks[i].x + blocks[i].w > p_win_w || grid->isOccupied(blocks[i].x, blocks[i].y, blocks[i].h))) {
+      return true;
     }
   }
+  return false;
 }
 
+void Piece::MoveLoc(int x, int y) {
+  //move down, left, right
+
+  for (int i = 3; i >= 0; i--) {
+    blocks[i].x += p_block_size*x;
+    blocks[i].y += p_block_size*y;
+  }
 
 
+}
+
+void Piece::Move(int x, int y, int r, Grid *grid) {
+  MoveLoc(x, y);
+
+  //left boundary check
+  while (OutOfBounds(false, true, false, grid) && x<0) {
+    MoveLoc(1, 0);
+  }
+
+  //right boundary check
+  while (OutOfBounds(false, false, true, grid) && x >0) {
+    MoveLoc(-1, 0);
+  }
+
+  //lower boundary check
+  while (OutOfBounds(true, false, false, grid) && y) {
+    MoveLoc(0, -1);
+    locked = true;
+  }
+  grid->CheckRows();
+  /*for (int i = 0; i < 4; i++) {
+    cout << "Move: location is (x: " << blocks[i].x << ", y: " << blocks[i].y << ")" << endl;
+  }*/
+
+}
